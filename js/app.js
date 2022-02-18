25e24c340e7fb79672bb6b27b2da5bc4d4313113
Al-Imran <alimran21feb@gmail.com>
2022-02-19 00:02:08 +0600

    js added

--- js/app.js
+++ js/app.js
@@ -1,27 +1,38 @@
 document.getElementById ('calculate-button').addEventListener ('click', function () {
-const incomeInput = document.getElementById ('income-input');
-const newIncomeAmountText = incomeInput.value;
-incomeInput.value = '';
-// console.log (newIncomeAmountText);
+    const incomeInput = document.getElementById ('income-input');
+    const incomeInputText = incomeInput.value;
+    let incomeAmount = parseFloat (incomeInputText);
+    // console.log (incomeAmount);
+    incomeInput.value = '';
 
-const foodInput = document.getElementById ('food-input');
-const newFoodAmountText = foodInput.value;
-foodInput.value = '';
-// console.log (newFoodAmountText);
+    const foodInput = document.getElementById ('food-input');
+    const foodInputText = foodInput.value;
+    let foodAmount = parseFloat (foodInputText);
+    // console.log (foodAmount);
+    foodInput.value = '';
 
-const rentInput = document.getElementById ('rent-input');
-const newRentAmountText = rentInput.value;
-rentInput.value = '';
-// console.log (newRentAmountText);
+    const rentInput = document.getElementById ('rent-input');
+    const rentInputText = rentInput.value;
+    let rentAmount = parseFloat (rentInputText);
+    // console.log (rentAmount);
+    rentInput.value = '';
 
-const clothesInput = document.getElementById ('clothes-input');
-const newClothesAmountText = clothesInput.value;
-clothesInput.value = '';
-console.log (newClothesAmountText);
+    const clothesInput = document.getElementById ('clothes-input');
+    const clothesInputText = clothesInput.value;
+    let clothesAmount = parseFloat (clothesInputText);
+    // console.log (clothesAmount)
+    clothesInput.value = '';
 
+    let totalExpenses = foodAmount + rentAmount + clothesAmount;
+    // console.log (totalExpenses);
 
-
-
-
-
+    let totalBalanceField = document.getElementById ('balance');
+    let totalBalanceText = totalBalanceField.innerText;
+    let balance = incomeAmount - totalExpenses;
+    totalBalanceField.innerText = balance;
+    
+    let totalExpensesField = document.getElementById ('total-expenses');
+    let totalExpensesText = totalExpensesField.innerText;
+    let expensesAmount = totalExpenses;
+    totalExpensesField.innerText = totalExpenses;
 });
